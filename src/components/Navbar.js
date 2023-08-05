7df8322aa3f04745b62cab994a026258dc3c6d2d
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-05 14:21:58 +0200

    Add workflow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -106,33 +106,33 @@ const Navbar = () => {
           {(ref) => (
             <div className="md:hidden" id="mobile-menu">
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a
-                  href="/services"
+                <Link
+                  to="/services"
                   className="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-blue-600"
                 >
                   Services
-                </a>
+                </Link>
 
-                <a
-                  href="/team"
+                <Link
+                  to="/team"
                   className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-blue-600 hover:text-white"
                 >
                   Team
-                </a>
+                </Link>
 
-                <a
-                  href="/blog"
+                <Link
+                  to="/blog"
                   className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-blue-600 hover:text-white"
                 >
                   Blog
-                </a>
+                </Link>
 
-                <a
-                  href="/testimonials"
+                <Link
+                  to="/testimonials"
                   className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-900 hover:text-white"
                 >
                   Testimonials
-                </a>
+                </Link>
               </div>
             </div>
           )}
