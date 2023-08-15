22c420d06dddbb039a5e878198ba4c219692afa1
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-16 00:24:39 +0200

    Add the key points for the partners

--- src/components/OrderedListPartners.js
+++ src/components/OrderedListPartners.js
@@ -3,7 +3,7 @@ import React from 'react'
 const OrderedListPartners = () => {
     const products = [
         { title: "Lucrative Earnings: Partnering with GotBackup opens the door to substantial earning potential. As a reseller, you'll have the opportunity to earn attractive commissions and bonuses for each successful referral and sale.", id: 1 },
-        { title: "Peace of Mind: With our comprehensive backup services, you can rest easy knowing that your business's vital data is shielded from unforeseen disasters, hardware failures, or cybersecurity threats.", id: 2 },
+        { title: "Trusted Brand: GotBackup is a reputable and trusted name in data security and backup solutions. Aligning your business with our established brand enhances your credibility and provides a competitive advantage.", id: 2 },
         { title: "Seamless Recovery: In the event of data loss, our seamless recovery process ensures that you can quickly restore your information, minimizing downtime and preventing potential business disruptions.", id: 3 },
         { title: "Tailored Solutions: We understand that every business's data needs are unique. Our solutions are customizable to fit your specific requirements, ensuring optimal protection without unnecessary complexities.", id: 4 },
         { title: "Trusted by Professionals: Trusted by businesses of all sizes, GotBackup is the go-to partner for data backup. Our industry expertise and satisfied clients stand as a testament to our commitment to data security.", id: 5 },
