c366deb475618ba49fe71a1de0fba8d503145175
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-22 01:11:56 +0200

    Add the the testimonies

--- src/components/pages/Testimonials.jsx
+++ src/components/pages/Testimonials.jsx
@@ -39,7 +39,7 @@ function Testimonials({ apiUrl }) {
       id: 1,
       by: "Raymond",
       testimony:
-        "I love Got! This is an amazing service and it has saved me and my small business so much time.",
+        "I love Gotb! This is an amazing service and it has saved me and my small business so much time.",
     },
     {
       id: 2,
