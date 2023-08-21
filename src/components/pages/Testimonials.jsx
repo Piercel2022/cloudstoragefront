9d8e5ec6e0b0f31d2f092077e2663c4d93a6b638
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-22 01:05:49 +0200

    Add the workflow

--- src/components/pages/Testimonials.jsx
+++ src/components/pages/Testimonials.jsx
@@ -45,7 +45,7 @@ function Testimonials({ apiUrl }) {
       id: 2,
       by: "Chyka",
       testimony:
-        "GotBackup is the ultimate cloud storage service for small business owners like me. I recommend it",
+        "GotBackup is the ultimate cloud storage service for small business owners like me. I recommend it for",
     },
     {
       id: 3,
