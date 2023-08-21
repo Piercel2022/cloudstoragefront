4445e8665fade9b6e591875eba88718f3e4001d0
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-22 01:25:20 +0200

    Add the the testimonies

--- src/components/pages/Testimonials.jsx
+++ src/components/pages/Testimonials.jsx
@@ -39,7 +39,7 @@ function Testimonials({ apiUrl }) {
       id: 1,
       by: "Raymond",
       testimony:
-        "I love Gotbackup! This is an amazing service and it has saved me and my small business so much time. I love so",
+        "I love Gotbackup! This is an amazing service and it has saved me and my small business so much time. I love so many",
     },
     {
       id: 2,
